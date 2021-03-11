42d74cc75260d832f4f732cf5336b5e113498b4c
sachinwakle <sbwakle@hotmail.com>
2021-03-11 09:33:09 +0530

    integrated screen and added carousel for room

--- src/components/RoomCarousel.js
+++ src/components/RoomCarousel.js
@@ -1,63 +1,35 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Carousel from "react-bootstrap/Carousel";
-import RoomTwo from '../resources/static/images/roomtwo.jfif'
-import RoomOne from '../resources/static/images/roomone.jfif'
+import RoomTwo from "../resources/static/images/roomtwo.jfif";
+import RoomOne from "../resources/static/images/roomone.jfif";
 import { makeStyles } from "@material-ui/core";
+import Carousel from "react-material-ui-carousel";
+import CarouselItem from "./CarouselItem";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      width: "100%",
-      marginBottom: theme.spacing(5),
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(24),
-      fontWeight: "bold",
-    },
-    listHeading: {
-      fontSize: theme.typography.pxToRem(20),
-      fontWeight: "bold",
-    },
-  }));
+  root: {
+    width: "100%",
+    marginBottom: theme.spacing(5),
+  },
+}));
 
 function RoomCarousel() {
-    const classes = useStyles();
+  const classes = useStyles();
+  var items = [
+    {
+      title: "Random Name #1",
+      image: RoomOne,
+    },
+    {
+      title: "Random Name #2",
+      image: RoomTwo,
+    },
+  ];
   return (
     <div className={classes.root}>
-      <Carousel>
-        <Carousel.Item>
-          <img className="d-block w-100" src={RoomTwo} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={RoomOne}
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={RoomTwo}
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Carousel animation="slide">
+        {items.map((item, i) => (
+          <CarouselItem key={i} {...item} />
+        ))}
       </Carousel>
     </div>
   );
