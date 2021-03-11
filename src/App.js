cfd563388e1d25b5da02d486d55e6b4e0401fcae
akshay-zagade <akshayzagade777@gmail.com>
2021-03-11 23:55:26 +0530

    Added carousel on overview page

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
         <CssBaseline />
         <div className="App">
           <AppRouter/>
-          {/* <EnquiryForm /> */}
         </div>
       </ThemeProvider>
     </React.Fragment>
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import EnquiryForm from "./EnquiryForm";
+import OverviewPageCarousel from './OverviewPageCarousel'
 
 function Overview() {
     return (
         <div>
             <EnquiryForm />
+            <OverviewPageCarousel />
         </div>
     )
 }
