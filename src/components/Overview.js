224216d00bc223235c45d28d06c9146a8aa8b389
akshay-zagade <akshayzagade777@gmail.com>
2021-03-14 22:48:48 +0530

    Added hotel overview component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HotelOverview from "./HotelOverview";
 import OverviewPageCarousel from "./OverviewPageCarousel";
 
 function Overview() {
   return (
     <div>
       <OverviewPageCarousel />
+      <HotelOverview />
     </div>
   );
 }
